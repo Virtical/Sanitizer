82ce3ee43014966cbb132ac5e2d96ba86ba17f40
shestopalov <shestopalov@skbkontur.ru>
2026-05-14 18:01:15 +0500

    Правки

--- Sanitizer.Api/Models/MessageRequest.cs
+++ Sanitizer.Api/Models/MessageRequest.cs
@@ -5,4 +5,26 @@ public class MessageRequest
     public string Text { get; set; } = string.Empty;
     public MessageType Type { get; set; }
     public string? OriginalMessageId { get; set; }
+
+    public static MessageRequest CreateSent(string text)
+        => new() 
+        {
+            Text = text,
+            Type = MessageType.Sent,
+        };
+    
+    public static MessageRequest CreateAnswer(string text)
+        => new() 
+        {
+            Text = text,
+            Type = MessageType.Answer,
+        };
+    
+    public static MessageRequest CreateSanitized(string text, string originalMessageId)
+        => new() 
+        {
+            Text = text,
+            OriginalMessageId = originalMessageId,
+            Type = MessageType.Sanitized,
+        };
 }
